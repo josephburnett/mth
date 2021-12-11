5b7295499fc0486fed34bab3aed6e86e09321c57
Joseph Burnett <josephburnett79@gmail.com>
2021-12-11 11:58:26 +0100

    Render practice problem.

--- mth.core/src/mth/core.cljs
+++ mth.core/src/mth/core.cljs
@@ -4,36 +4,103 @@
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]))
 
-(println "This text is printed from src/mth/core.cljs. Go ahead and edit it and see reloading in action.")
-
 (defn multiply [a b] (* a b))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+
+;; Descriptions of the practice problems.
+
+(def practices
+  [{:key :adding-double-digits
+    :description "Adding double digit numbers"}])
+
+
+;; Functions to create new problems.
+
+(defmulti new-problem :key)
+
+(defn make-choices [n]
+  (let [lower (max 0 (- n 5))
+        upper (+ n 5)
+        incorrect-choices (for [m (range lower upper)
+                                :when (not= m n)] m)]
+    (->> incorrect-choices
+         (shuffle)
+         (take 4)
+         (cons n)
+         (shuffle))))
+
+(defmethod new-problem :adding-double-digits [practice]
+  (let [a (+ 10 (rand-int 89))
+        b (+ 10 (rand-int 89))
+        answer (+ a b)
+        choices (make-choices answer)]
+    (merge practice {:a a :b b :answer answer :choices choices})))
+
+
+;; Functions to update app state.
+
+(defonce app-state (atom {}))
+
+(defn click-practice [practice]
+  (let [state @app-state]
+    (reset!
+     app-state
+     (-> state
+         (assoc :practice practice)
+         (assoc :problem (new-problem practice))))))
+
+(defn click-response [response]
+  (let [state @app-state]
+    (reset!
+     app-state
+     (if (= (get-in state [:problem :answer]) response)
+       (assoc state :problem (new-problem (:practice state))) ; correct
+       (assoc-in state [:problem :response] response)))))     ; incorrect
+
+
+;; Functions to display the app and current problem.
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(defn hello-world []
+(defn render-choice [choice]
+  [:li {:on-click #(click-response choice)} choice])
+                                        
+(defn render-choices [problem]
+  (into [:ul] (for [c (:choices problem)]
+                (render-choice c))))
+
+(defmulti render-problem :key)
+
+(defmethod render-problem :adding-double-digits [problem]
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/mth/core.cljs and watch it change!"]])
+   [:span (:a problem) "+" (:b problem) "="]
+   [render-choices problem]])
+
+(defn render-practice [practice]
+  [:div
+   [:span [:a {:on-click #(click-practice practice)} (:description practice)]]])
+
+(defn render-practice-selection []
+  (into [:div] (map render-practice practices)))
+
+(defn app []
+  (let [state @app-state]
+    [:div
+     [:h1 (:text @app-state)]
+     (if (:problem state)
+       [render-problem (:problem state)]
+       [render-practice-selection])]))
 
 (defn mount [el]
-  (rdom/render [hello-world] el))
+  (rdom/render [app] el))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
     (mount el)))
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
 
-;; specify reload hook with ^:after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
